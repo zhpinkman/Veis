b2e2c5d9c3ed3b3b4834fe3a0eebdbc111c6dc2d
Ebrahim <meghasemim1999@gmail.com>
2018-07-27 14:41:05 +0430

    Ts Config Path

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { RESTANGULAR_AUTH } from '../restangular.config';
+import { RESTANGULAR_AUTH } from '@app/restangular.config';
 
 
 @Injectable({
