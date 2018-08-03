cea8d3f4c01938c02f4a8b09d7d635306514e9a2
zhivar <zh.sourati@ut.ac.ir>
2018-08-03 23:09:43 +0430

    email validation corrected

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,19 +1,27 @@
-import { LoginComponent } from '@app/login/login.component';
-import { UtilitiesService } from '@app/Services/utilities.service';
-import { AuthService } from './../Services/auth.service';
-import { User } from './../User';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormControlDirective, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { LoginComponent } from "@app/login/login.component";
+import { UtilitiesService } from "@app/Services/utilities.service";
+import { AuthService } from "./../Services/auth.service";
+import { User } from "./../User";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormControlDirective,
+  Validators
+} from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
-
-  constructor(private authService: AuthService,private utils: UtilitiesService,private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private utils: UtilitiesService,
+    private router: Router
+  ) {}
   registerForm: FormGroup;
   username: FormControl;
   name: FormControl;
@@ -21,51 +29,59 @@ export class SignupComponent implements OnInit {
   password: FormControl;
   bucket: FormControl;
   ngOnInit() {
-      this.createFormControls();
-      this.createRegisterForm();
+    this.createFormControls();
+    this.createRegisterForm();
   }
-  createRegisterForm(){
+  createRegisterForm() {
     this.registerForm = new FormGroup({
       email: this.email,
       password: this.password,
-      bucket : this.bucket
+      bucket: this.bucket
     });
   }
-  createFormControls(){
-    this.email =  new FormControl('',[Validators.required, Validators.pattern("[^ @+-]*@(ut.ac.ir)$")]);
-    this.password =  new FormControl('', Validators.required);
-    this.bucket =  new FormControl('', Validators.required);
+  createFormControls() {
+    this.email = new FormControl("", [
+      Validators.required,
+      Validators.pattern("[^ @+-]*@(ut.ac.ir)$")
+    ]);
+    this.password = new FormControl("", Validators.required);
+    this.bucket = new FormControl("", Validators.required);
   }
 
-  bucketDefaultGenerator(){
-    let position = this.email.value.indexOf('@');
-    if(position == -1){
+  bucketDefaultGenerator() {
+    let position = this.email.value.indexOf("@");
+    if (position == -1) {
       return this.email.value;
-    }
-    else{
+    } else {
       return this.email.value.substring(0, position);
     }
   }
 
-  bucketGenerator(){
+  bucketGenerator() {
     this.bucket.setValue(this.bucketDefaultGenerator());
     this.bucket.patchValue(this.bucketDefaultGenerator());
   }
-  
 
-
-
-  signup(){
-    let newUser: User = { email: this.email.value, password: this.password.value, bucketName: this.bucket.value};
-    this.authService.signupRequest(newUser).subscribe(
-      res => {
-        this.utils.success("success", "Signed up successfully");
-        this.router.navigate(["/login"])
-      },
-      error => {
-        if(error.status == 409)
-          this.utils.error("Failed", "Email or Bucket exists")
-      }
-    );
+  signup() {
+    if (this.registerForm.invalid) {
+      this.utils.error("Failed", "please fill the from in the correct format!!");
+    }
+    else {
+      let newUser: User = {
+        email: this.email.value,
+        password: this.password.value,
+        bucketName: this.bucket.value
+      };
+      this.authService.signupRequest(newUser).subscribe(
+        res => {
+          this.utils.success("success", "Signed up successfully");
+          this.router.navigate(["/login"]);
+        },
+        error => {
+          if (error.status == 409)
+            this.utils.error("Failed", "Email or Bucket exists");
+        }
+      );
+    }
   }
 }
