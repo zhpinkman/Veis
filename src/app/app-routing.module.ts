aacbdcd4c45843d4b682f0ab33e049b135d88f4c
zhivar <zh.sourati@ut.ac.ir>
2018-08-03 12:46:57 +0430

    notifications and navigation after register added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '@app/login/login.component';
 
-const routes: Routes = [{ path: 'signup', component: SignupComponent },
+const routes: Routes = [
+{ path: 'signup', component: SignupComponent },
 { path: 'login', component: LoginComponent}]
 
 @NgModule({
