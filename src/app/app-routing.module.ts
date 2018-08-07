8e17c3a23ff0a61c1902716769d7cc10e8e56f6e
Vahid Mohammadi <email@vmoh.ir>
2018-08-07 11:37:46 +0430

    home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,25 @@
 import { CompactFileComponent } from './compact-file/compact-file.component';
 import { HomeComponent } from '@app/home/home.component';
 import { AuthGuard } from '@app/Services/authguard';
-import {SignupComponent} from '@app/signup/signup.component';
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {LoginComponent} from '@app/login/login.component';
-import {UploadFileComponent} from '@app/upload-file/upload-file.component';
+import { SignupComponent } from '@app/signup/signup.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '@app/login/login.component';
+import { UploadFileComponent } from '@app/upload-file/upload-file.component';
 import { FilesListComponent } from '@app/files-list/files-list.component';
 
 const routes: Routes = [
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'upload', component: UploadFileComponent, canActivate: [AuthGuard]},
-  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: 'file', component: CompactFileComponent, canActivate: [AuthGuard]},
-  {path: 'list', component: FilesListComponent, canActivate: [AuthGuard]},
-  {path: 'list/:id', component: FilesListComponent}
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'upload', component: UploadFileComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'file', component: CompactFileComponent, canActivate: [AuthGuard] },
+  { path: 'list', component: FilesListComponent, canActivate: [AuthGuard] },
+  { path: 'list/:id', component: FilesListComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
