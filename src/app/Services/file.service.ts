a6c5c0e360f97800e1cee753ca669f5fa5de8848
zhivar <zh.sourati@ut.ac.ir>
2018-08-07 12:04:12 +0430

    children of home added in app-routing

--- src/app/Services/file.service.ts
+++ src/app/Services/file.service.ts
@@ -1,17 +1,21 @@
-import { Injectable, Inject } from '@angular/core';
-import { RESTANGULAR_AUTH } from '@app/restangular.config';
+import { Injectable, Inject } from "@angular/core";
+import { RESTANGULAR_AUTH } from "@app/restangular.config";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class FileService {
   constructor(@Inject(RESTANGULAR_AUTH) private restangular) {}
 
   makeRequest(formData) {
-    this.restangular.one('file/upload').customPOST(formData, undefined, undefined, { 'Content-Type': undefined });
+    this.restangular
+      .one("file/upload")
+      .customPOST(formData, undefined, undefined, {
+        "Content-Type": undefined
+      });
   }
 
-  getFiles(){
-    return this.restangular.one('file/all').get();
+  getFiles() {
+    return this.restangular.one("file/all").get();
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,31 @@
-import { CompactFileComponent } from './compact-file/compact-file.component';
-import { HomeComponent } from '@app/home/home.component';
-import { AuthGuard } from '@app/Services/authguard';
-import { SignupComponent } from '@app/signup/signup.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from '@app/login/login.component';
-import { UploadFileComponent } from '@app/upload-file/upload-file.component';
-import { FilesListComponent } from '@app/files-list/files-list.component';
+import { CompactFileComponent } from "./compact-file/compact-file.component";
+import { HomeComponent } from "@app/home/home.component";
+import { AuthGuard } from "@app/Services/authguard";
+import { SignupComponent } from "@app/signup/signup.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "@app/login/login.component";
+import { UploadFileComponent } from "@app/upload-file/upload-file.component";
+import { FilesListComponent } from "@app/files-list/files-list.component";
 
 const routes: Routes = [
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'upload', component: UploadFileComponent, canActivate: [AuthGuard] },
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'file', component: CompactFileComponent, canActivate: [AuthGuard] },
-  { path: 'list', component: FilesListComponent, canActivate: [AuthGuard] },
-  { path: 'list/:id', component: FilesListComponent }
+  { path: "signup", component: SignupComponent },
+  { path: "login", component: LoginComponent },
+  { path: "upload", component: UploadFileComponent, canActivate: [AuthGuard] },
+  {
+    path: "",
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: "file",
+        component: CompactFileComponent,
+        canActivate: [AuthGuard]
+      },
+      { path: "list", component: FilesListComponent, canActivate: [AuthGuard] },
+      { path: "list/:id", component: FilesListComponent }
+    ]
+  }
 ];
 
 @NgModule({
