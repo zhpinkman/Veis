232f6b53fcd27a84aaffb137fe9979f9e0c91503
zhivar <zh.sourati@ut.ac.ir>
2018-08-08 03:07:47 +0430

    something like animation added to delete button

--- src/app/full-file/full-file.component.ts
+++ src/app/full-file/full-file.component.ts
@@ -1,33 +1,41 @@
-import { humanizeBytes } from "ngx-uploader";
-import { Component, OnInit, Inject, Pipe, PipeTransform } from "@angular/core";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
-import { FileEntity } from "@app/file";
-import { ConstService } from "@app/Services/const.service";
+import { humanizeBytes } from 'ngx-uploader';
+import { Component, OnInit, Inject, Pipe, PipeTransform } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FileEntity } from '@app/file';
+import { ConstService } from '@app/Services/const.service';
 
 @Component({
-  selector: "app-full-file",
-  templateUrl: "./full-file.component.html",
-  styleUrls: ["./full-file.component.scss"]
+  selector: 'app-full-file',
+  templateUrl: './full-file.component.html',
+  styleUrls: ['./full-file.component.scss']
 })
 export class FullFileComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: FileEntity, public consts: ConstService) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: FileEntity,
+    public consts: ConstService
+  ) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
   showEditName: boolean = false;
+  hideDeleteIcon: Boolean = false;
 
   icons = {
-    txt: " fa-file text-info ",
-    jpg: " text-warning fa-image ",
-    dir: " fa-folder text-primary ",
-    cpp: " fa-code text-danger ",
-    pdf: " fa-file-pdf-o text-danger "
+    txt: ' fa-file text-info ',
+    jpg: ' text-warning fa-image ',
+    dir: ' fa-folder text-primary ',
+    cpp: ' fa-code text-danger ',
+    pdf: ' fa-file-pdf-o text-danger '
   };
   submit() {
     this.showEditName = !this.showEditName;
   }
+
+  delete() {
+    this.hideDeleteIcon = true;
+  }
 }
 
-@Pipe({ name: "size" })
+@Pipe({ name: 'size' })
 export class SizeHandler implements PipeTransform {
   humanizeBytes: Function;
   constructor() {
@@ -36,6 +44,4 @@ export class SizeHandler implements PipeTransform {
   transform(value: number): string {
     return this.humanizeBytes(value);
   }
-
-
 }
