8915d06af4d6daca96086c3aa859e580f1b203c1
zhivar <zh.sourati@ut.ac.ir>
2018-08-08 12:22:52 +0430

    header component updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,38 @@
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { NgxUploaderModule } from "ngx-uploader";
-import { LoginComponent } from "@app/login/login.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "@app/app-routing.module";
-import { AppComponent } from "@app/app.component";
-import { AuthService } from "@app/Services/auth.service";
-import { RestangularModule, Restangular } from "ngx-restangular";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxUploaderModule } from 'ngx-uploader';
+import { LoginComponent } from '@app/login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { AuthService } from '@app/Services/auth.service';
+import { RestangularModule, Restangular } from 'ngx-restangular';
 import {
   RESTANGULAR_AUTH,
   RestangularAuthFactory,
   RestangularConfigFactory,
   RESTANGULAR_NOT_AUTH,
   RestangularNotAuthFactory
-} from "@app/restangular.config";
-import { SignupComponent } from "@app/signup/signup.component";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { UploadFileComponent } from "@app/upload-file/upload-file.component";
-import { SimpleNotificationsModule } from "angular2-notifications";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HomeComponent } from "@app/home/home.component";
-import { HeaderComponent } from "@app/header/header.component";
-import { CompactFileComponent } from "./compact-file/compact-file.component";
+} from '@app/restangular.config';
+import { SignupComponent } from '@app/signup/signup.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { UploadFileComponent } from '@app/upload-file/upload-file.component';
+import { SimpleNotificationsModule } from 'angular2-notifications';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from '@app/home/home.component';
+import { HeaderComponent } from '@app/header/header.component';
+import { CompactFileComponent } from './compact-file/compact-file.component';
 import {
   FullFileComponent,
   SizeHandler
-} from "./full-file/full-file.component";
-import { MatDialogModule } from "@angular/material/dialog";
-import { FilesListComponent } from "./files-list/files-list.component";
-import { UploadItemComponent } from "./upload-item/upload-item.component";
-import { MatProgressBarModule } from "@angular/material";
-import { MatCardModule } from "@angular/material/card";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatButtonModule } from "@angular/material/button";
+} from './full-file/full-file.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FilesListComponent } from './files-list/files-list.component';
+import { UploadItemComponent } from './upload-item/upload-item.component';
+import { MatProgressBarModule, MatFormFieldModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -50,6 +50,7 @@ import { MatButtonModule } from "@angular/material/button";
   ],
 
   imports: [
+    MatFormFieldModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
@@ -63,7 +64,7 @@ import { MatButtonModule } from "@angular/material/button";
     MatProgressBarModule,
     MatCardModule,
     MatGridListModule,
-    MatButtonModule,
+    MatButtonModule
   ],
   entryComponents: [FullFileComponent],
   providers: [
@@ -80,4 +81,4 @@ import { MatButtonModule } from "@angular/material/button";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
