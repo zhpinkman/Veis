7ceda70ed39a27ded79d2e3e3c8ede7b3c6adea9
mehrab_bp <mbpmbp1998@gmail.com>
2018-09-13 11:46:40 +0430

    resolved copu cut bugs

--- src/app/files-list/files-list.component.ts
+++ src/app/files-list/files-list.component.ts
@@ -68,7 +68,10 @@ export class FilesListComponent implements OnInit {
       // console.log('value', value);
       if (this.fileService.filePasted) {
         // console.log('bool', this.fileService.copiedFiles.includes(value));
-        if (!this.fileService.copiedFiles.includes(value)) {
+        if (
+          this.fileService.copiedFiles.findIndex(k => k.name == value.name) ==
+          -1
+        ) {
           // console.log('copyfiles', this.fileService.copiedFiles);
           this.fileService.copiedFiles.push(value);
         }
